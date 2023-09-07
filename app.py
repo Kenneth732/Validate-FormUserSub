827224b88bdfeb6fcd5f0f06c2a89ecc9024ce20
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-07 14:53:34 +0300

    index form validate

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
-app.route('/')
+app.config['SECRET_KEY'] = '86987815d884ef414253fac91dd187339165a9500f25d1d5'
+
+
+courses_list = [{
+    'title': 'Python 101',
+    'description': 'Learn Python basics',
+    'price': 34,
+    'available': True,
+    'level': 'Beginner'
+    }]
+
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    return render_template('base.html')
+    form = CourseForm()
+    return render_template('index.html', form=form)
